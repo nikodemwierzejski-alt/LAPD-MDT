219c7ab27b1ba04997dddcdffbef9466c37c3b6f
nikodemwierzejski-alt <nikodemwierzejski@gmail.com>
2026-07-20 21:28:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const { Client } = require("pg");
 const cors = require("cors");
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('.'));
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
 const db = new Client({
     connectionString: "postgresql://neondb_owner:npg_T94GvMwZcjyA@ep-old-bread-auu10o6b.c-10.us-east-1.aws.neon.tech/neondb?sslmode=require"
