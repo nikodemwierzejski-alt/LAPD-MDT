8cf010179578d4563c50a3c384cca231370f358e
nikodemwierzejski-alt <nikodemwierzejski@gmail.com>
2026-07-21 05:03:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const { Client } = require("pg");
 const cors = require("cors");
 
 const app = express();
-app.use(express.static('.'));
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
